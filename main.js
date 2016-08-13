2920a418cdd7bd631b6b8a7a7feb5512c2a33f28
Svinci131 <svinci131@gmail.com>
2016-08-13 14:04:50 -0400

    numbers work

--- main.js
+++ main.js
@@ -13,6 +13,25 @@ function getImageTags () {
 	});
 }
 
+function isDate (string) {
+	var months = {
+		January: 31, 
+		February: 28,
+		March: 31,
+		April: 30,
+		May: 31,
+		June: 30,
+		July: 31,
+		August: 31,
+		September: 30,
+		October: 31,
+		November: 30,
+		December: 31 
+	}
+	if (months[string]) return true
+	else return false
+}
+
 function messWithNumbers () {
 	var body = document.getElementById('mw-content-text')
 	var els = body.childNodes
@@ -21,11 +40,11 @@ function messWithNumbers () {
 		var el = els[i]
 		var text = el.innerHTML ? el.innerHTML.split(" ") : [];
 		var newText = []; 
-		text.forEach(function(word){
+		text.forEach(function(word, i){
 			if (!isNaN(word)) {
-				var newNum = parseInt(word)+500
+				var monthCheck = text[i-2]
+				var newNum = parseInt(word)+10
 				newText.push(newNum)
-				console.log(word, isNaN(word))
 			}
 			else {
 				newText.push(word)
@@ -33,46 +52,5 @@ function messWithNumbers () {
 		});
 
 		if (newText.length) el.innerHTML = newText.join(" ")
-	}
-
-	
-
-	console.log(els, els[3])
-	//add 100 to all numbers greater then 100
-	// for(var i = 0; i < paragraphs.length; i++) {
-	// 	var newContent = [];
-	// 	text = paragraphs[i].innerHTML.split(" "); 
-	// 	text.forEach(function(word) {
-	// 		// console.log(word, isNaN(word))
-	// 		if (!isNaN(word)) {
-	// 			console.log(word, isNaN(word))
-	// 		}
-	// 		else {
-	// 			newContent.push(word);
-	// 		}
-	// 	});
-	// 	newContent = newContent.join()
-	// 	console.log(newContent)
-	// 	paragraphs[i].innerHTML = newContent;
-	// 	//only over 100
-		
-	// }
+	}	
 }
-// function getSimilarImage () {
-// 	var API_KEY = "fe03e6cf12d2f0ad16e81c15cc926317";
-// 	var API_BASE = 'https://api.flickr.com/services/rest/';
-// 	var method 	= 'flickr.photos.search',
-// }
-
-//http://developer.streak.com/2014/10/how-to-use-gmail-api-in-chrome-extension.html
-//https://developers.google.com/image-search/v1/devguide
-
-
-// OAuth client
-// Here is your client ID
-// 675415601767-59gk5fhqalir1n38q9o4vtamam6m7tmm.apps.googleusercontent.com
-// Here is your client secret
-// h5vwNPsxFREJZ7d_NcB4mEMB
-// API key
-// Here is your API key
-// AIzaSyCOkaVMDvRtk7CuwBZhXE6gzedH-tx71Yg
