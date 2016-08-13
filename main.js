b14b11a1b4ff11ae6e65cf50ed81b2d37812c68a
Svinci131 <svinci131@gmail.com>
2016-08-13 13:37:49 -0400

    start

--- main.js
+++ main.js
@@ -1,23 +1,58 @@
-
-// document.querySelectorAll("h1")[0].innerHTML = "my text"
-
-
-//Function that search dom and get all image links 
-//Function that uses the api gets tags 
-	//Gets similar images
-//Replaces
-
 getCredentials(); 
 var mainImage = getMainImageSrc(); 
 var accessToken = localStorage.getItem('accessToken');
-getImageTags ()
+getImageTags();
+messWithNumbers ()
+
+
 function getImageTags () {
 	postImage(mainImage)
 	.then(function(tags) {
 		console.log(tags)
+		getSimilarImages()
 	});
 }
 
-function getSimilarImage () {
-	
+function messWithNumbers () {
+	var body = document.getElementById('mw-content-text')
+	var els = body.childNodes
+	els[3].innerHTML = "hi"
+	console.log()
+	//add 100 to all numbers greater then 100
+	// for(var i = 0; i < paragraphs.length; i++) {
+	// 	var newContent = [];
+	// 	text = paragraphs[i].innerHTML.split(" "); 
+	// 	text.forEach(function(word) {
+	// 		// console.log(word, isNaN(word))
+	// 		if (!isNaN(word)) {
+	// 			console.log(word, isNaN(word))
+	// 		}
+	// 		else {
+	// 			newContent.push(word);
+	// 		}
+	// 	});
+	// 	newContent = newContent.join()
+	// 	console.log(newContent)
+	// 	paragraphs[i].innerHTML = newContent;
+	// 	//only over 100
+		
+	// }
 }
+// function getSimilarImage () {
+// 	var API_KEY = "fe03e6cf12d2f0ad16e81c15cc926317";
+// 	var API_BASE = 'https://api.flickr.com/services/rest/';
+// 	var method 	= 'flickr.photos.search',
+// }
+
+//http://developer.streak.com/2014/10/how-to-use-gmail-api-in-chrome-extension.html
+//https://developers.google.com/image-search/v1/devguide
+
+
+// OAuth client
+// Here is your client ID
+// 675415601767-59gk5fhqalir1n38q9o4vtamam6m7tmm.apps.googleusercontent.com
+// Here is your client secret
+// h5vwNPsxFREJZ7d_NcB4mEMB
+// API key
+// Here is your API key
+// AIzaSyCOkaVMDvRtk7CuwBZhXE6gzedH-tx71Yg
