e884b002a1b9b2e823ebe3fc4b44913d4fb5ecc6
Svinci131 <svinci131@gmail.com>
2016-09-17 20:16:52 -0400

    deleting extra files refactoring

--- main.js
+++ main.js
@@ -1,84 +1,71 @@
 _ini();
-
 function _ini(){
 
     document.getElementsByTagName("html")[0].style.visibility="hidden";
+    
+    window.onload = function(){
 
-    window.onload=function(){
+      var mainImage = document.querySelector("a.image > img");
 
-    var foundImage = document.querySelector("a.image > img")
-
-    function getMainImageSrc() {
-      return foundImage.src
-    }
-    //replace that image's source with a new source'
-    function replaceImage(image, url){
-
-      document.querySelector("a.image > img").src=url;
-      document.querySelector("a.image > img").srcset=url;
-      document.querySelector("a.image > img").style.display="block";
-      document.querySelector("a.image > img").style.width="auto";
-      document.querySelector("a.image > img").style.height="auto";
-      document.querySelector("a.image > img").style.margin="0 auto";
-
-    }
+      getCredentials();
+      messWithNumbers();  
 
 
+      postImage(mainImage.src)
+      .then(function(tags) {
+        var truncTags = tags.slice(0,5) || tags;
+        //remove adult for obvious reasons
+        var adult = truncTags.indexOf("adult")
+        truncTags.splice(adult, 1)
+        return getBingData(truncTags);
+      })
+      .then(function(url){
+        replaceImage(mainImage, url);
+        document.getElementsByTagName("html")[0].style.visibility="initial";
+      })
+      .catch(function (err) {
+        console.log(err)
+      });
 
-        //do your stuff
-var mainImage = getMainImageSrc();
+      //replace that image's source with a new source'
+      function replaceImage(image, url) {
 
-getCredentials();
-messWithNumbers();
+        document.querySelector("a.image > img").src=url;
+        document.querySelector("a.image > img").srcset=url;
+        document.querySelector("a.image > img").style.display="block";
+        document.querySelector("a.image > img").style.width="auto";
+        document.querySelector("a.image > img").style.height="auto";
+        document.querySelector("a.image > img").style.margin="0 auto";
+      }
 
+      function isPainting (tags) {
+      	var paintingWords = {
+      		"art": true,
+      		"painting": true
+      	}
+      	for(var i = 0; i < tags.length; i++) {
+      		if (paintingWords[tags[i]]) return true
+      	}
+      	return false
+      }
 
-postImage(mainImage)
-.then(function(tags) {
-  var truncTags = tags.slice(0,5) || tags;
-  //remove adult for obvious reasons
-  var adult = truncTags.indexOf("adult")
-  truncTags.splice(adult, 1)
-  return getBingData(truncTags);
-})
-.then(function(url){
-  replaceImage(foundImage, url);
-       document.getElementsByTagName("html")[0].style.visibility="initial";
-
-})
-.catch(function (err) {
-  console.log(err)
-});
-
-function isPainting (tags) {
-	var paintingWords = {
-		"art": true,
-		"painting": true
-	}
-	for(var i = 0; i < tags.length; i++) {
-		if (paintingWords[tags[i]]) return true
-	}
-	return false
-}
-
-function getBingData (tags) {
-	var url = "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q="
-  + tags.join(", ")
-  + ", &count=1&offset=0&mkt=en-us&safeSearch=Moderate"
-  console.log("------------------------------")
-  console.log(url)
-  console.log("------------------------------")
-	return axios.get(url, {
-    headers: {
-      "Ocp-Apim-Subscription-Key": "3fc3d83a1e7c44bca46c097afcaeb748"
-    }
-  })
-  	.then(function(r) {
-  		console.log("cats!", r.data.value[0]);
-  		return r.data.value[0].thumbnailUrl
-  	})
-}
-
-
+      function getBingData (tags) {
+      	var url = "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q="
+        + tags.join(", ")
+        + ", &count=1&offset=0&mkt=en-us&safeSearch=Moderate"
+        console.log("------------------------------")
+        console.log(url)
+        console.log("------------------------------")
+      	return axios.get(url, {
+          headers: {
+            "Ocp-Apim-Subscription-Key": "3fc3d83a1e7c44bca46c097afcaeb748"
+          }
+        })
+      	.then(function(r) {
+      		console.log("cats!", r.data.value[0]);
+      		return r.data.value[0].thumbnailUrl
+      	})
+      }
     }
 
 }
